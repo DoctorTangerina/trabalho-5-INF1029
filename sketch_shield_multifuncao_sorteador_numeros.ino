87eb81fabca850c791f4111464ad239e8a1ea7ba
MTuro <marcosturo69@gmail.com>
2025-11-22 18:13:31 -0300

    raffle

--- sketch_shield_multifuncao_sorteador_numeros.ino
+++ sketch_shield_multifuncao_sorteador_numeros.ino
@@ -77,6 +77,102 @@ void Interrupt()
 void Raffle()
 {
   
+  if (rangeFlag != RANGE_SET) { 
+    MFS.write("----");
+    delay(300);
+    return;
+  }
+
+  
+  randomSeed(analogRead(A0));
+  int value = random(currentMin, currentMax + 1);
+
+  currentState = GENERATOR_STARTED;
+  
+  MFS.beep(5, 0, 1, 1, 0);
+
+  
+  int digits[4];
+  digits[0] = value % 10;
+  digits[1] = (value / 10) % 10;
+  digits[2] = (value / 100) % 10;
+  digits[3] = (value / 1000) % 10;
+
+  bool interrupted = false;
+
+ 
+  for (int stage = 0; stage < 4 && !interrupted; ++stage) {
+    for (int cnt = 9; cnt >= 0 && !interrupted; --cnt) {
+      
+      int toShow = 0;
+      int mul = 1;
+      for (int pos = 0; pos < 4; ++pos) {
+        int out;
+        if (pos < stage) {
+          out = digits[pos];
+        } else {
+          out = cnt;
+        }
+        toShow += out * mul;
+        mul *= 10;
+      }
+      MFS.write(toShow);
+
+      // animacao
+      int ledStep = (9 - cnt) % 5;
+      switch (ledStep) {
+        case 0: MFS.led(1, LOW); MFS.led(2, LOW); MFS.led(3, LOW); MFS.led(4, LOW); break;
+        case 1: MFS.led(1, HIGH); MFS.led(2, LOW); MFS.led(3, LOW); MFS.led(4, LOW); break;
+        case 2: MFS.led(1, LOW); MFS.led(2, HIGH); MFS.led(3, LOW); MFS.led(4, LOW); break;
+        case 3: MFS.led(1, LOW); MFS.led(2, LOW); MFS.led(3, HIGH); MFS.led(4, LOW); break;
+        case 4: MFS.led(1, LOW); MFS.led(2, LOW); MFS.led(3, LOW); MFS.led(4, HIGH); break;
+      }
+
+      unsigned long start = millis();
+      while (millis() - start < 100) {
+        byte btn = MFS.getButton();
+        if (btn) {
+          byte buttonNumber = btn & B00111111;
+          byte buttonAction = btn & B11000000;
+          if (buttonNumber == 1 && buttonAction == BUTTON_SHORT_RELEASE_IND) {
+            interrupted = true;
+            break;
+          }
+        }
+        delay(5);
+      }
+    }
+    
+    int fixedVal = 0; int m = 1;
+    for (int p = 0; p < 4; ++p) {
+      int o = (p <= stage) ? digits[p] : 0;
+      fixedVal += o * m; m *= 10;
+    }
+    MFS.write(fixedVal);
+    delay(50);
+  }
+
+  if (interrupted) {
+    MFS.write("Intr");
+    
+    MFS.led(1, LOW); MFS.led(2, LOW); MFS.led(3, LOW); MFS.led(4, LOW);
+    
+    MFS.beep(5,5,3,1,0);
+    rangeFlag = RANGE_NOT_SET;
+    currentState = GENERATOR_STOPPED;
+    return;
+  }
+
+   MFS.write(value);
+  for (int i = 0; i < 3; ++i) {
+    delay(200);
+    MFS.write("    ");
+    delay(200);
+    MFS.write(value);
+  }
+   MFS.beep(50,50,3,1,0);
+  MFS.led(1, LOW); MFS.led(2, LOW); MFS.led(3, LOW); MFS.led(4, LOW);
+  currentState = GENERATOR_STOPPED;
 }
 
 void shortAction3()
